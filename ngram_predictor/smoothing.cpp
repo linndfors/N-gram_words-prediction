61983ee54e18271d8700c15aa342198275261e31
maslenchenko <o.maslenchenko@ucu.edu.ua>
2023-04-19 15:15:48 +0300

    fixed issues

--- ngram_predictor/smoothing.cpp
+++ ngram_predictor/smoothing.cpp
@@ -1,35 +1,28 @@
 #include "smoothing.h"
 
 double smoothing(ngram::ngram_dict_t ngrams_dict, ngram::ngram_t ngram, double d) {
+
     int frequency = ngrams_dict[ngram];
     double first_term_nom = std::max(frequency-d, 0.0);
 
-    // put given ngram together, do not include last word
-    std::string ngram_phrase = "";
-    for (int i = 0; i < ngram.size()-1; ++i) {
-        ngram_phrase += ngram[i];
-        ngram_phrase += " ";
-    }
-
-    int first_term_denom = 0;
-    int possible_next_words = 0; //counter for (n-1gram *)
+    double first_term_denom = 0;
+    double possible_next_words = 0; //counter for (n-1gram *)
     std::string final_word = ngram[ngram.size()-1]; //last word in given ngram
-    int final_word_count = 0;
+    double final_word_count = 0;
 
     for (const auto& [curr_ngram, freq] : ngrams_dict) {
-        std::string phrase = "";
-        //put together current ngram
-        for (int i = 0; i < curr_ngram.size(); ++i) {
-            phrase += curr_ngram[i];
-            phrase += " ";
+
+        size_t reduced_ngram_size = curr_ngram.size()-1;
+        for (int i = 0; i < reduced_ngram_size; ++i) {
+            if (curr_ngram[i] != ngram[i]) break;
+            if (i == reduced_ngram_size-1) {
+                first_term_denom += freq;
+                ++possible_next_words;
+            }
         }
-        //check if n-1 given ngram is in the current ngram
-        if (phrase.find(ngram_phrase) != std::string::npos) {
-            first_term_denom += freq;
-            ++possible_next_words;
-        }
+
         //check if the last word is in current ngram
-        if (phrase.find(final_word) != std::string::npos) {
+        if (final_word == curr_ngram[curr_ngram.size() - 1]) {
             ++final_word_count;
         }
     }
