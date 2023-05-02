974db5f2c80061081ffbb121959674101c74f430
linndfors <olha.nahurna@ucu.edu.ua>
2023-05-02 12:52:56 +0200

    database fro words-id

--- ngram_predictor/ngram_predictor.hpp
+++ ngram_predictor/ngram_predictor.hpp
@@ -30,6 +30,15 @@ struct std::hash<std::vector<int>> {
     }
 };
 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+    int i;
+    for(i = 0; i<argc; i++) {
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+}
+
 
 class ngram_predictor {
 public:
