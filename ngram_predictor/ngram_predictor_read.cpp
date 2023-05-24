a1a38748d80b193e4b49d72ac3c61eafe53ea58d
Yaryna_Fialko <yaryna.fialko@ucu.edu.ua>
2023-05-24 15:07:08 +0300

    empty sentence ignore

--- ngram_predictor/ngram_predictor_read.cpp
+++ ngram_predictor/ngram_predictor_read.cpp
@@ -154,15 +154,20 @@ void ngram_predictor::count_ngrams_in_str(std::string &file_content) {
     ngram_dict_id_tbb::accessor a;
 
     for (const auto &sentence : sentence_index) {
+        // Split into words
+        bl::boundary::ssegment_index words_index(bl::boundary::word, sentence.begin(), sentence.end());
+        words_index.rule(bl::boundary::word_letters);
+
+        // Skip empty sentences
+        if (words_index.begin() == words_index.end()) {
+            continue;
+        }
+
         // Add <s> tag at the beginning of the sentence
         ngram_id temp_ngram;
         for (int i = 0; i < m_n - 1; ++i) {
             temp_ngram.emplace_back(START_TAG_ID);
         }
-
-        // Split into words
-        bl::boundary::ssegment_index words_index(bl::boundary::word, sentence.begin(), sentence.end());
-        words_index.rule(bl::boundary::word_letters);
 
         for (const auto &word : words_index) {
             temp_ngram.emplace_back(convert_to_id(word, true));
